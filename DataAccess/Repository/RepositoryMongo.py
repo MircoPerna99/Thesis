70611578f65db122278892b9c6599e9850438fc6
MircoPerna <132653588+MircoPerna99@users.noreply.github.com>
2025-11-01 18:57:46 +0100

    Try fix

--- DataAccess/Repository/RepositoryMongo.py
+++ DataAccess/Repository/RepositoryMongo.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("../"))
 from pymongo import MongoClient
 from DataAccess.Model.PPI_Model import PPIModel
 from DataAccess.Model.DTI_Model import DTIModel
