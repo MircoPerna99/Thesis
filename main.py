58725a99a4da6ab974227597221d3805eb0a0079
MircoPerna <132653588+MircoPerna99@users.noreply.github.com>
2025-11-07 20:33:37 +0100

    Add avg CV

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from ML_Models.Analyses import applyAnlyses
 from ML_Models.Analyses import applyCrossValidation
+from ML_Models.Analyses import analysysBiologicalNetwork
 #applyAnlyses()
 applyCrossValidation()
+#analysysBiologicalNetwork()
